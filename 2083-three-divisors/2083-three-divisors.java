3e1aac3d7db2957a647546b8d4cbbb02c5395794
DanishaDS <danisha.ds00@gmail.com>
2025-02-03 11:29:49 +0530

    Time: 1 ms (62.43%), Space: 41 MB (6.3%) - LeetHub

---
+++ 2083-three-divisors/2083-three-divisors.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isThree(int n) {
+        int i,c=0;
+        for(i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                c++;
+            }
+            
+        }
+        if(c==3)
+        {
+            return true;
+        }
+        return false;
+    }
+}
